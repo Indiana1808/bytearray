1ae82cca3a1619f2ae18e90dfaff136f8cf76d28
Indiana1808 <jenitatindiana@yahoo.com>
2020-10-01 20:16:52 +0400

    parité impair

---
+++ bytearray1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+def parite(a):
+	countn = 0 
+	resultat = []
+	for i in a:
+		countn = 0
+		print("\n")
+		for j in i:
+			print(j,' ','1' in j)
+			if '1' in j:
+				countn = countn + 1
+		print("le nombre de 1 est =", countn, "\n")
+		if countn%2==0:
+			resultat.append("1"+j)
+		else:
+			resultat.append("0"+i)
+	return resultat
+
+b = ["111000", "11011"]
+print(parite(b))
+
